10c4e9944339791d01198ed46095841055d90f11
bilawal <bilawalnadeem981@gmail.com>
2026-04-27 17:22:56 +0500

    Add login and signup fully functional

--- Hr-Frontend/src/assets/icons/JobOpenings/icon.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { FaEdit, FaEye } from "react-icons/fa";
-
-export const EditIcon = () => <FaEdit className="w-6 h-6" />;
-export const ViewIcon = () => <FaEye className="w-6 h-6" />;
